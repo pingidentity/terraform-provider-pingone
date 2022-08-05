ab9bcbfe88d1dbd5926614cd45bab20815503941
Patrick Cowland <patrickcowland@pingidentity.com>
2022-08-05 18:19:15 +0100

    `pingone_application_role_assignment`

---
+++ internal/service/sso/resource_application_role_assignment.go
@@ -0,0 +1,248 @@
+package sso
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/patrickcping/pingone-go-sdk-v2/management"
+	client "github.com/pingidentity/terraform-provider-pingone/internal/client"
+)
+
+func ResourceApplicationRoleAssignment() *schema.Resource {
+	return &schema.Resource{
+
+		// This description is used by the documentation generator and the language server.
+		Description: "Resource to create and manage PingOne admin role assignments to applications.",
+
+		CreateContext: resourcePingOneApplicationRoleAssignmentCreate,
+		ReadContext:   resourcePingOneApplicationRoleAssignmentRead,
+		DeleteContext: resourcePingOneApplicationRoleAssignmentDelete,
+
+		Importer: &schema.ResourceImporter{
+			StateContext: resourcePingOneApplicationRoleAssignmentImport,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"environment_id": {
+				Description:      "The ID of the environment.",
+				Type:             schema.TypeString,
+				Required:         true,
+				ForceNew:         true,
+				ValidateDiagFunc: validation.ToDiagFunc(validation.StringIsNotEmpty),
+			},
+			"application_id": {
+				Description:      "The ID of an application to assign an admin role to.",
+				Type:             schema.TypeString,
+				Required:         true,
+				ForceNew:         true,
+				ValidateDiagFunc: validation.ToDiagFunc(validation.StringIsNotEmpty),
+			},
+			"role_id": {
+				Description:      "The ID of an admin role to assign to the application.",
+				Type:             schema.TypeString,
+				Required:         true,
+				ForceNew:         true,
+				ValidateDiagFunc: validation.ToDiagFunc(validation.StringIsNotEmpty),
+			},
+			"scope_organization_id": {
+				Description:   "Limit the scope of the admin role assignment to the specified organisation ID.",
+				Type:          schema.TypeString,
+				Optional:      true,
+				ForceNew:      true,
+				ConflictsWith: []string{"scope_environment_id", "scope_population_id"},
+			},
+			"scope_environment_id": {
+				Description:   "Limit the scope of the admin role assignment to the specified environment ID.",
+				Type:          schema.TypeString,
+				Optional:      true,
+				ForceNew:      true,
+				ConflictsWith: []string{"scope_organization_id", "scope_population_id"},
+			},
+			"scope_population_id": {
+				Description:   "Limit the scope of the admin role assignment to the specified population ID.",
+				Type:          schema.TypeString,
+				Optional:      true,
+				ForceNew:      true,
+				ConflictsWith: []string{"scope_organization_id", "scope_environment_id"},
+			},
+			"read_only": {
+				Description: "A flag to show whether the admin role assignment is read only or can be changed.",
+				Type:        schema.TypeBool,
+				Computed:    true,
+			},
+		},
+	}
+}
+
+func resourcePingOneApplicationRoleAssignmentCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	p1Client := meta.(*client.Client)
+	apiClient := p1Client.API.ManagementAPIClient
+	ctx = context.WithValue(ctx, management.ContextServerVariables, map[string]string{
+		"suffix": p1Client.API.Region.URLSuffix,
+	})
+
+	var diags diag.Diagnostics
+
+	//d.Get("scope_id").(string)
+
+	scopeID := ""
+	scopeType := ""
+	if organisationID, ok := d.GetOk("scope_organization_id"); ok {
+		scopeID = organisationID.(string)
+		scopeType = "ORGANIZATION"
+
+	} else if environmentID, ok := d.GetOk("scope_environment_id"); ok {
+		scopeID = environmentID.(string)
+		scopeType = "ENVIRONMENT"
+
+	} else if populationID, ok := d.GetOk("scope_population_id"); ok {
+		scopeID = populationID.(string)
+		scopeType = "POPULATION"
+
+	} else {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "One of scope_organization_id, scope_environment_id or scope_population_id must be set",
+			Detail:   "One of scope_organization_id, scope_environment_id or scope_population_id must be set",
+		})
+
+		return diags
+	}
+
+	applicationRoleAssignmentRole := *management.NewRoleAssignmentRole(d.Get("role_id").(string))
+	applicationRoleAssignmentScope := *management.NewRoleAssignmentScope(scopeID, management.EnumRoleAssignmentScopeType(scopeType))
+	applicationRoleAssignment := *management.NewRoleAssignment(applicationRoleAssignmentRole, applicationRoleAssignmentScope) // UserRoleAssignment |  (optional)
+
+	resp, r, err := apiClient.UsersUserRoleAssignmentsApi.CreateUserRoleAssignment(ctx, d.Get("environment_id").(string), d.Get("application_id").(string)).RoleAssignment(applicationRoleAssignment).Execute()
+	if (err != nil) || (r.StatusCode != 201) {
+
+		response := &management.P1Error{}
+		errDecode := json.NewDecoder(r.Body).Decode(response)
+		if errDecode == nil {
+			diags = append(diags, diag.Diagnostic{
+				Severity: diag.Warning,
+				Summary:  fmt.Sprintf("Cannot decode error response: %v", errDecode),
+				Detail:   fmt.Sprintf("Full HTTP response: %v\n", r.Body),
+			})
+		}
+
+		if r.StatusCode == 400 && response.GetDetails()[0].GetTarget() == "scope" {
+			diags = diag.FromErr(fmt.Errorf("Incompatible role and scope combination. Role: %s / Scope: %s", applicationRoleAssignmentRole.GetId(), applicationRoleAssignmentScope.GetType()))
+
+			return diags
+		}
+
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  fmt.Sprintf("Error when calling `UsersUserRoleAssignmentsApi.CreateUserRoleAssignment``: %v", err),
+			Detail:   fmt.Sprintf("Full HTTP response: %v\n", r.Body),
+		})
+
+		return diags
+	}
+
+	d.SetId(resp.GetId())
+
+	return resourcePingOneApplicationRoleAssignmentRead(ctx, d, meta)
+}
+
+func resourcePingOneApplicationRoleAssignmentRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	p1Client := meta.(*client.Client)
+	apiClient := p1Client.API.ManagementAPIClient
+	ctx = context.WithValue(ctx, management.ContextServerVariables, map[string]string{
+		"suffix": p1Client.API.Region.URLSuffix,
+	})
+	var diags diag.Diagnostics
+
+	resp, r, err := apiClient.UsersUserRoleAssignmentsApi.ReadOneUserRoleAssignment(ctx, d.Get("environment_id").(string), d.Get("application_id").(string), d.Id()).Execute()
+	if err != nil {
+
+		if r.StatusCode == 404 {
+			log.Printf("[INFO] PingOne Role Assignment %s no longer exists", d.Id())
+			d.SetId("")
+			return nil
+		}
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  fmt.Sprintf("Error when calling `UsersUserRoleAssignmentsApi.ReadOneRoleAssignment``: %v", err),
+			Detail:   fmt.Sprintf("Full HTTP response: %v\n", r.Body),
+		})
+
+		return diags
+	}
+
+	d.Set("role_id", resp.GetRole().Id)
+	d.Set("read_only", resp.GetReadOnly())
+
+	if resp.GetScope().Type == "ORGANIZATION" {
+		d.Set("scope_organization_id", resp.GetScope().Id)
+		d.Set("scope_environment_id", nil)
+		d.Set("scope_population_id", nil)
+
+	} else if resp.GetScope().Type == "ENVIRONMENT" {
+		d.Set("scope_organization_id", nil)
+		d.Set("scope_environment_id", resp.GetScope().Id)
+		d.Set("scope_population_id", nil)
+
+	} else if resp.GetScope().Type == "POPULATION" {
+		d.Set("scope_organization_id", nil)
+		d.Set("scope_environment_id", nil)
+		d.Set("scope_population_id", resp.GetScope().Id)
+	}
+
+	return diags
+}
+
+func resourcePingOneApplicationRoleAssignmentDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	p1Client := meta.(*client.Client)
+	apiClient := p1Client.API.ManagementAPIClient
+	ctx = context.WithValue(ctx, management.ContextServerVariables, map[string]string{
+		"suffix": p1Client.API.Region.URLSuffix,
+	})
+	var diags diag.Diagnostics
+
+	if d.Get("read_only").(bool) {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  fmt.Sprintf("Role assignment %s cannot be deleted as it is read only", d.Id()),
+		})
+
+		return diags
+	}
+
+	_, err := apiClient.UsersUserRoleAssignmentsApi.DeleteUserRoleAssignment(ctx, d.Get("environment_id").(string), d.Get("application_id").(string), d.Id()).Execute()
+	if err != nil {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  fmt.Sprintf("Error when calling `UsersUserRoleAssignmentsApi.DeleteUserRoleAssignment``: %v", err),
+		})
+
+		return diags
+	}
+
+	return nil
+}
+
+func resourcePingOneApplicationRoleAssignmentImport(ctx context.Context, d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, error) {
+	attributes := strings.SplitN(d.Id(), "/", 3)
+
+	if len(attributes) != 3 {
+		return nil, fmt.Errorf("invalid id (\"%s\") specified, should be in format \"environmentID/applicationID/roleAssignmentID\"", d.Id())
+	}
+
+	environmentID, applicationID, roleAssignmentID := attributes[0], attributes[1], attributes[2]
+
+	d.Set("environment_id", environmentID)
+	d.Set("application_id", applicationID)
+	d.SetId(roleAssignmentID)
+
+	resourcePingOneApplicationRoleAssignmentRead(ctx, d, meta)
+
+	return []*schema.ResourceData{d}, nil
+}
---
+++ internal/service/sso/resource_application_role_assignment_test.go
@@ -0,0 +1,302 @@
+package sso_test
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	pingone "github.com/patrickcping/pingone-go-sdk-v2/management"
+	"github.com/pingidentity/terraform-provider-pingone/internal/acctest"
+)
+
+func testAccCheckRoleAssignmentApplicationDestroy(s *terraform.State) error {
+	var ctx = context.Background()
+
+	p1Client, err := acctest.TestClient(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	apiClient := p1Client.API.ManagementAPIClient
+	ctx = context.WithValue(ctx, pingone.ContextServerVariables, map[string]string{
+		"suffix": p1Client.API.Region.URLSuffix,
+	})
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "pingone_application_role_assignment" {
+			continue
+		}
+
+		_, rEnv, err := apiClient.EnvironmentsApi.ReadOneEnvironment(ctx, rs.Primary.Attributes["environment_id"]).Execute()
+
+		if rEnv.StatusCode == 404 {
+			continue
+		}
+
+		if err != nil {
+			return err
+		}
+
+		body, r, err := apiClient.ApplicationsApplicationRoleAssignmentsApi.ReadOneApplicationRoleAssignment(ctx, rs.Primary.Attributes["environment_id"], rs.Primary.Attributes["application_id"], rs.Primary.ID).Execute()
+
+		if r.StatusCode == 404 {
+			continue
+		}
+
+		if err != nil {
+			tflog.Error(ctx, fmt.Sprintf("Error: %v", body))
+			return err
+		}
+
+		return fmt.Errorf("PingOne Application Role Assignment %s still exists", rs.Primary.ID)
+	}
+
+	return nil
+}
+
+func TestAccRoleAssignmentApplication_Population(t *testing.T) {
+	t.Parallel()
+
+	resourceName := acctest.ResourceNameGen()
+	resourceFullName := fmt.Sprintf("pingone_application_role_assignment.%s", resourceName)
+
+	environmentName := acctest.ResourceNameGenEnvironment()
+
+	licenseID := os.Getenv("PINGONE_LICENSE_ID")
+	region := os.Getenv("PINGONE_REGION")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { acctest.PreCheckEnvironment(t) },
+		ProviderFactories: acctest.ProviderFactories,
+		CheckDestroy:      testAccCheckRoleAssignmentApplicationDestroy,
+		ErrorCheck:        acctest.ErrorCheck(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccRoleAssignmentApplicationConfig_Population(environmentName, resourceName, "Identity Data Admin", licenseID, region),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(resourceFullName, "id"),
+					resource.TestCheckResourceAttrSet(resourceFullName, "environment_id"),
+					resource.TestCheckResourceAttrSet(resourceFullName, "application_id"),
+					resource.TestCheckResourceAttrSet(resourceFullName, "role_id"),
+					resource.TestCheckResourceAttrSet(resourceFullName, "scope_population_id"),
+					resource.TestCheckResourceAttr(resourceFullName, "scope_organization_id", ""),
+					resource.TestCheckResourceAttr(resourceFullName, "scope_environment_id", ""),
+					resource.TestCheckResourceAttr(resourceFullName, "read_only", "false"),
+				),
+			},
+			{
+				Config:      testAccRoleAssignmentApplicationConfig_Population(environmentName, resourceName, "Environment Admin", licenseID, region),
+				ExpectError: regexp.MustCompile(`Incompatible role and scope combination. Role: [a-z0-9\-]* \/ Scope: POPULATION`),
+			},
+			{
+				Config:      testAccRoleAssignmentApplicationConfig_Population(environmentName, resourceName, "Organization Admin", licenseID, region),
+				ExpectError: regexp.MustCompile(`Incompatible role and scope combination. Role: [a-z0-9\-]* \/ Scope: POPULATION`),
+			},
+		},
+	})
+}
+
+func TestAccRoleAssignmentApplication_Organisation(t *testing.T) {
+	t.Parallel()
+
+	resourceName := acctest.ResourceNameGen()
+	resourceFullName := fmt.Sprintf("pingone_application_role_assignment.%s", resourceName)
+
+	environmentName := acctest.ResourceNameGenEnvironment()
+
+	licenseID := os.Getenv("PINGONE_LICENSE_ID")
+	region := os.Getenv("PINGONE_REGION")
+	organisationID := os.Getenv("PINGONE_ORGANIZATION_ID")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { acctest.PreCheckEnvironment(t) },
+		ProviderFactories: acctest.ProviderFactories,
+		CheckDestroy:      testAccCheckRoleAssignmentApplicationDestroy,
+		ErrorCheck:        acctest.ErrorCheck(t),
+		Steps: []resource.TestStep{
+			{
+				Config:      testAccRoleAssignmentApplicationConfig_Organisation(environmentName, resourceName, "Identity Data Admin", licenseID, region, organisationID),
+				ExpectError: regexp.MustCompile(`Incompatible role and scope combination. Role: [a-z0-9\-]* \/ Scope: ORGANIZATION`),
+			},
+			{
+				Config: testAccRoleAssignmentApplicationConfig_Organisation(environmentName, resourceName, "Environment Admin", licenseID, region, organisationID),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(resourceFullName, "id"),
+					resource.TestCheckResourceAttrSet(resourceFullName, "environment_id"),
+					resource.TestCheckResourceAttrSet(resourceFullName, "application_id"),
+					resource.TestCheckResourceAttrSet(resourceFullName, "role_id"),
+					resource.TestCheckResourceAttr(resourceFullName, "scope_population_id", ""),
+					resource.TestCheckResourceAttrSet(resourceFullName, "scope_organization_id"),
+					resource.TestCheckResourceAttr(resourceFullName, "scope_environment_id", ""),
+					resource.TestCheckResourceAttr(resourceFullName, "read_only", "false"),
+				),
+			},
+			{
+				Config: testAccRoleAssignmentApplicationConfig_Organisation(environmentName, resourceName, "Organization Admin", licenseID, region, organisationID),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(resourceFullName, "id"),
+					resource.TestCheckResourceAttrSet(resourceFullName, "environment_id"),
+					resource.TestCheckResourceAttrSet(resourceFullName, "application_id"),
+					resource.TestCheckResourceAttrSet(resourceFullName, "role_id"),
+					resource.TestCheckResourceAttr(resourceFullName, "scope_population_id", ""),
+					resource.TestCheckResourceAttrSet(resourceFullName, "scope_organization_id"),
+					resource.TestCheckResourceAttr(resourceFullName, "scope_environment_id", ""),
+					resource.TestCheckResourceAttr(resourceFullName, "read_only", "false"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccRoleAssignmentApplication_Environment(t *testing.T) {
+	t.Parallel()
+
+	resourceName := acctest.ResourceNameGen()
+	resourceFullName := fmt.Sprintf("pingone_application_role_assignment.%s", resourceName)
+
+	environmentName := acctest.ResourceNameGenEnvironment()
+
+	licenseID := os.Getenv("PINGONE_LICENSE_ID")
+	region := os.Getenv("PINGONE_REGION")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { acctest.PreCheckEnvironmentAndOrganisation(t) },
+		ProviderFactories: acctest.ProviderFactories,
+		CheckDestroy:      testAccCheckRoleAssignmentApplicationDestroy,
+		ErrorCheck:        acctest.ErrorCheck(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccRoleAssignmentApplicationConfig_Environment(environmentName, resourceName, "Identity Data Admin", licenseID, region),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(resourceFullName, "id"),
+					resource.TestCheckResourceAttrSet(resourceFullName, "environment_id"),
+					resource.TestCheckResourceAttrSet(resourceFullName, "application_id"),
+					resource.TestCheckResourceAttrSet(resourceFullName, "role_id"),
+					resource.TestCheckResourceAttr(resourceFullName, "scope_population_id", ""),
+					resource.TestCheckResourceAttr(resourceFullName, "scope_organization_id", ""),
+					resource.TestCheckResourceAttrSet(resourceFullName, "scope_environment_id"),
+					resource.TestCheckResourceAttr(resourceFullName, "read_only", "false"),
+				),
+			},
+			{
+				Config: testAccRoleAssignmentApplicationConfig_Environment(environmentName, resourceName, "Environment Admin", licenseID, region),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(resourceFullName, "id"),
+					resource.TestCheckResourceAttrSet(resourceFullName, "environment_id"),
+					resource.TestCheckResourceAttrSet(resourceFullName, "application_id"),
+					resource.TestCheckResourceAttrSet(resourceFullName, "role_id"),
+					resource.TestCheckResourceAttr(resourceFullName, "scope_population_id", ""),
+					resource.TestCheckResourceAttr(resourceFullName, "scope_organization_id", ""),
+					resource.TestCheckResourceAttrSet(resourceFullName, "scope_environment_id"),
+					resource.TestCheckResourceAttr(resourceFullName, "read_only", "false"),
+				),
+			},
+			{
+				Config:      testAccRoleAssignmentApplicationConfig_Environment(environmentName, resourceName, "Organization Admin", licenseID, region),
+				ExpectError: regexp.MustCompile(`Incompatible role and scope combination. Role: [a-z0-9\-]* \/ Scope: ENVIRONMENT`),
+			},
+		},
+	})
+}
+
+func testAccRoleAssignmentApplicationConfig_Population(environmentName, resourceName, roleName, licenseID, region string) string {
+	return fmt.Sprintf(`
+		resource "pingone_environment" "%[1]s" {
+			name = "%[1]s"
+			type = "SANDBOX"
+			license_id = "%[4]s"
+			region = "%[5]s"
+			default_population {}
+			service {}
+		}
+
+		resource "pingone_application" "%[2]s" {
+			environment_id = "${pingone_environment.%[1]s.id}"
+			population_id = "${pingone_environment.%[1]s.default_population_id}"
+
+			applicationname = "%[2]s"
+			email    = "fooapplication@pingidentity.com"
+		}
+
+		data "pingone_role" "%[2]s" {
+			name = "%[3]s"
+		}
+
+		resource "pingone_application_role_assignment" "%[2]s" {
+			environment_id = "${pingone_environment.%[1]s.id}"
+			application_id = "${pingone_application.%[2]s.id}"
+			role_id = "${data.pingone_role.%[2]s.id}"
+
+			scope_population_id = "${pingone_environment.%[1]s.default_population_id}"
+		}`, environmentName, resourceName, roleName, licenseID, region)
+}
+
+func testAccRoleAssignmentApplicationConfig_Organisation(environmentName, resourceName, roleName, licenseID, region, organisationID string) string {
+	return fmt.Sprintf(`
+		resource "pingone_environment" "%[1]s" {
+			name = "%[1]s"
+			type = "SANDBOX"
+			license_id = "%[4]s"
+			region = "%[5]s"
+			default_population {}
+			service {}
+		}
+
+		resource "pingone_application" "%[2]s" {
+			environment_id = "${pingone_environment.%[1]s.id}"
+			population_id = "${pingone_environment.%[1]s.default_population_id}"
+
+			applicationname = "%[2]s"
+			email    = "fooapplication@pingidentity.com"
+		}
+
+		data "pingone_role" "%[2]s" {
+			name = "%[3]s"
+		}
+
+		resource "pingone_application_role_assignment" "%[2]s" {
+			environment_id = "${pingone_environment.%[1]s.id}"
+			application_id = "${pingone_application.%[2]s.id}"
+			role_id = "${data.pingone_role.%[2]s.id}"
+
+			scope_organization_id = "%[6]s"
+		}`, environmentName, resourceName, roleName, licenseID, region, organisationID)
+}
+
+func testAccRoleAssignmentApplicationConfig_Environment(environmentName, resourceName, roleName, licenseID, region string) string {
+	return fmt.Sprintf(`
+		resource "pingone_environment" "%[1]s" {
+			name = "%[1]s"
+			type = "SANDBOX"
+			license_id = "%[4]s"
+			region = "%[5]s"
+			default_population {}
+			service {}
+		}
+
+		resource "pingone_application" "%[2]s" {
+			environment_id = "${pingone_environment.%[1]s.id}"
+			population_id = "${pingone_environment.%[1]s.default_population_id}"
+
+			applicationname = "%[2]s"
+			email    = "fooapplication@pingidentity.com"
+		}
+
+		data "pingone_role" "%[2]s" {
+			name = "%[3]s"
+		}
+
+		resource "pingone_application_role_assignment" "%[2]s" {
+			environment_id = "${pingone_environment.%[1]s.id}"
+			application_id = "${pingone_application.%[2]s.id}"
+			role_id = "${data.pingone_role.%[2]s.id}"
+
+			scope_environment_id = "${pingone_environment.%[1]s.id}"
+		}`, environmentName, resourceName, roleName, licenseID, region)
+}
