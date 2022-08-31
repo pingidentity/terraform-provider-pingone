637c193b90becb9076e40a9fb4765508e80cb862
Patrick Cowland <patrickcowland@pingidentity.com>
2022-08-31 19:38:58 +0100

    docs

--- examples/resources/pingone_key/resource.tf
+++ examples/resources/pingone_key/resource.tf
@@ -5,14 +5,14 @@ resource "pingone_environment" "my_environment" {
 resource "pingone_key" "my_key" {
   environment_id = pingone_environment.my_environment.id
 
-  name = "%[4]s"
+  name       = "%[4]s"
   subject_dn = "CN=%[4]s, OU=Ping Identity, O=Ping Identity, L=, ST=, C=US"
 
-		algorithm = "RSA"
-		key_length = 3072
-		signature_algorithm = "SHA512withRSA"
+  algorithm           = "RSA"
+  key_length          = 3072
+  signature_algorithm = "SHA512withRSA"
 
-		usage_type = "ENCRYPTION"
+  usage_type = "ENCRYPTION"
 
-    validity_period = 360
+  validity_period = 360
 }
