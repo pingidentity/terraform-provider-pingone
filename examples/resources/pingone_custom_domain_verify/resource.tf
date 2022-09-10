f37f77debeba6b30ad57738de97cb9b4272fcfb9
Patrick Cowland <patrickcowland@pingidentity.com>
2022-09-10 10:42:36 +0100

    docs

---
+++ examples/resources/pingone_custom_domain_verify/resource.tf
@@ -0,0 +1,5 @@
+resource "pingone_custom_domain_verify" "my_custom_domain_verification" {
+  environment_id = pingone_environment.my_environment.id
+
+  custom_domain_id = pingone_custom_domain.my_custom_domain.id
+}
