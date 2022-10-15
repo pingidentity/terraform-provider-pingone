d4a6e0b54fc213fd53340b1881f4817c4a8696dc
Patrick Cowland <44225864+patrickcping@users.noreply.github.com>
2022-10-15 17:53:17 +0100

    correct trusted email domain examples (#189)

--- examples/data-sources/pingone_trusted_email_domain_dkim/data-source.tf
+++ examples/data-sources/pingone_trusted_email_domain_dkim/data-source.tf
@@ -1,5 +1,5 @@
 data "pingone_trusted_email_domain_dkim" "email_domain_dkim" {
   environment_id = pingone_environment.my_environment.id
 
-  trusted_email_domain_id = pingone_trusted_email_domain.id
+  trusted_email_domain_id = pingone_trusted_email_domain.my_custom_email_domain.id
 }
--- examples/data-sources/pingone_trusted_email_domain_spf/data-source.tf
+++ examples/data-sources/pingone_trusted_email_domain_spf/data-source.tf
@@ -1,5 +1,5 @@
 data "pingone_trusted_email_domain_spf" "email_domain_spf" {
   environment_id = pingone_environment.my_environment.id
 
-  trusted_email_domain_id = pingone_trusted_email_domain.id
+  trusted_email_domain_id = pingone_trusted_email_domain.my_custom_email_domain.id
 }
