bf61b0028d5194ae635657a4d011ad1936433743
Patrick Cowland <44225864+patrickcping@users.noreply.github.com>
2023-04-18 19:46:40 +0100

    Correct test failure race condition (#338)
    * Correct testing race condition
    
    * terrafmt

--- internal/service/sso/resource_resource_attribute_test.go
+++ internal/service/sso/resource_resource_attribute_test.go
@@ -456,6 +456,9 @@ resource "pingone_resource_attribute" "%[3]s" {
 
   name  = "email"
   value = "$${user.%[4]s}"
+  depends_on = [
+    pingone_schema_attribute.%[3]s
+  ]
 }`, acctest.MinimalSandboxEnvironment(environmentName, licenseID), environmentName, resourceName, name)
 }
 
