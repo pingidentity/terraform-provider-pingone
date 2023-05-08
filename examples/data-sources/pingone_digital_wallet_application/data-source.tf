3f345d06f5cd63fad43ae86898c23fc914a0e428
mjspi <mikesimon@pingidentity.com>
2023-05-08 16:01:09 -0500

    Added datasource examples.

--- examples/data-sources/pingone_digital_wallet_application/data-source.tf
+++ examples/data-sources/pingone_digital_wallet_application/data-source.tf
@@ -1,9 +1,9 @@
 data "pingone_digital_wallet_application" "example_by_id" {
-  environment_id = var.environment_id
+  environment_id    = var.environment_id
   digital_wallet_id = var.application_id
 }
 
-data "pingone_digital_wallet_application" "example_by_digital_wallet_name" {  
+data "pingone_digital_wallet_application" "example_by_digital_wallet_name" {
   environment_id = var.environment_id
   name           = "foo"
 }
