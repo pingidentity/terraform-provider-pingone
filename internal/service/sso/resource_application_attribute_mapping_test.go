f7876dd3c0538f09aa6df5d26c4b6925ac9b818e
Patrick Cowland <patrickcowland@pingidentity.com>
2023-08-08 08:56:29 +0100

    add application resource attribute mapping test

--- internal/service/sso/resource_application_attribute_mapping_test.go
+++ internal/service/sso/resource_application_attribute_mapping_test.go
@@ -67,7 +67,7 @@ func testAccCheckApplicationAttributeMappingDestroy(s *terraform.State) error {
 	return nil
 }
 
-func testAccGetMFAPolicyIDs(resourceName string, environmentID, resourceID *string) resource.TestCheckFunc {
+func testAccGetApplicationAttributeMappingIDs(resourceName string, environmentID, applicationID, resourceID *string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 
 		rs, ok := s.RootModule().Resources[resourceName]
@@ -76,32 +76,33 @@ func testAccGetMFAPolicyIDs(resourceName string, environmentID, resourceID *stri
 		}
 
 		*resourceID = rs.Primary.ID
+		*applicationID = rs.Primary.Attributes["application_id"]
 		*environmentID = rs.Primary.Attributes["environment_id"]
 
 		return nil
 	}
 }
 
-func TestAccMFAPolicy_RemovalDrift(t *testing.T) {
+func TestAccApplicationAttributeMapping_RemovalDrift(t *testing.T) {
 	t.Parallel()
 
 	resourceName := acctest.ResourceNameGen()
-	resourceFullName := fmt.Sprintf("pingone_mfa_policy.%s", resourceName)
+	resourceFullName := fmt.Sprintf("pingone_application_attribute_mapping.%s", resourceName)
 
 	name := resourceName
 
-	var resourceID, environmentID string
+	var resourceID, applicationID, environmentID string
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { acctest.PreCheckEnvironment(t) },
 		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
-		CheckDestroy:             testAccCheckMFAPolicyDestroy,
+		CheckDestroy:             testAccCheckApplicationAttributeMappingDestroy,
 		ErrorCheck:               acctest.ErrorCheck(t),
 		Steps: []resource.TestStep{
 			// Configure
 			{
-				Config: testAccMFAPolicyConfig_FullSMS(resourceName, name),
-				Check:  testAccGetMFAPolicyIDs(resourceFullName, &environmentID, &resourceID),
+				Config: testAccApplicationAttributeMappingConfig_OIDC_Minimal(resourceName, name),
+				Check:  testAccGetApplicationAttributeMappingIDs(resourceFullName, &environmentID, &applicationID, &resourceID),
 			},
 			// Replan after removal preconfig
 			{
@@ -113,15 +114,15 @@ func TestAccMFAPolicy_RemovalDrift(t *testing.T) {
 						t.Fatalf("Failed to get API client: %v", err)
 					}
 
-					apiClient := p1Client.API.MFAAPIClient
+					apiClient := p1Client.API.ManagementAPIClient
 
 					if environmentID == "" || resourceID == "" {
-						t.Fatalf("One of environment ID or resource ID cannot be determined. Environment ID: %s, Resource ID: %s", environmentID, resourceID)
+						t.Fatalf("One of environment ID, application ID or resource ID cannot be determined. Environment ID: %s, Application ID: %s, Resource ID: %s", environmentID, applicationID, resourceID)
 					}
 
-					_, err = apiClient.DeviceAuthenticationPolicyApi.DeleteDeviceAuthenticationPolicy(ctx, environmentID, resourceID).Execute()
+					_, err = apiClient.ApplicationAttributeMappingApi.DeleteApplicationAttributeMapping(ctx, environmentID, applicationID, resourceID).Execute()
 					if err != nil {
-						t.Fatalf("Failed to delete MFA Policy: %v", err)
+						t.Fatalf("Failed to delete Application attribute mapping: %v", err)
 					}
 				},
 				RefreshState:       true,
