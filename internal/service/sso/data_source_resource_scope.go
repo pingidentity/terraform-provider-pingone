2c5d4ff821d67b6ad5900e251099668e4cb63033
Patrick Cowland <patrickcowland@pingidentity.com>
2023-09-11 12:54:06 +0100

    `data-source/pingone_resource_scope`: Migrated to plugin framework.

--- internal/service/sso/data_source_resource_scope.go
+++ internal/service/sso/data_source_resource_scope.go
@@ -4,219 +4,338 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"strings"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/patrickcping/pingone-go-sdk-v2/management"
-	client "github.com/pingidentity/terraform-provider-pingone/internal/client"
+	"github.com/pingidentity/terraform-provider-pingone/internal/framework"
 	"github.com/pingidentity/terraform-provider-pingone/internal/sdk"
 	"github.com/pingidentity/terraform-provider-pingone/internal/verify"
 )
 
-func DatasourceResourceScope() *schema.Resource {
-	return &schema.Resource{
+// Types
+type ResourceScopeDataSource serviceClientType
 
+type ResourceScopeDataSourceModel struct {
+	Id               types.String `tfsdk:"id"`
+	EnvironmentId    types.String `tfsdk:"environment_id"`
+	ResourceId       types.String `tfsdk:"resource_id"`
+	ResourceScopeId  types.String `tfsdk:"resource_scope_id"`
+	Name             types.String `tfsdk:"name"`
+	Description      types.String `tfsdk:"description"`
+	SchemaAttributes types.Set    `tfsdk:"schema_attributes"`
+	MappedClaims     types.Set    `tfsdk:"mapped_claims"`
+}
+
+// Framework interfaces
+var (
+	_ datasource.DataSource = &ResourceScopeDataSource{}
+)
+
+// New Object
+func NewResourceScopeDataSource() datasource.DataSource {
+	return &ResourceScopeDataSource{}
+}
+
+// Metadata
+func (r *ResourceScopeDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_resource_scope"
+}
+
+// Schema
+func (r *ResourceScopeDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+
+	nameLength := 1
+
+	resourceScopeIdDescription := framework.SchemaAttributeDescriptionFromMarkdown(
+		"The ID of the resource scope.",
+	).ExactlyOneOf([]string{"resource_scope_id", "name"}).AppendMarkdownString("Must be a valid PingOne resource ID.")
+
+	nameDescription := framework.SchemaAttributeDescriptionFromMarkdown(
+		"The name of the resource scope.",
+	).ExactlyOneOf([]string{"resource_scope_id", "name"})
+
+	schemaAttributesDescription := framework.SchemaAttributeDescriptionFromMarkdown(
+		"A list that specifies the user schema attributes that can be read or updated for the specified PingOne access control scope. The value is an array of schema attribute paths (such as `username`, `name.given`, `shirtSize`) that the scope controls. This property is supported only for the `p1:read:user`, `p1:update:user` and `p1:read:user:{suffix}` and `p1:update:user:{suffix}` scopes. No other PingOne platform scopes allow this behavior. Any attributes not listed in the attribute array are excluded from the read or update action. The wildcard path (`*`) in the array includes all attributes and cannot be used in conjunction with any other user schema attribute path.",
+	)
+
+	mappedClaimsDescription := framework.SchemaAttributeDescriptionFromMarkdown(
+		"A list of custom resource attribute IDs. This property applies only for the resource with its type property set to `OPENID_CONNECT`. Moreover, this property does not display predefined OpenID Connect (OIDC) mappings, such as the `email` claim in the OIDC `email` scope or the `name` claim in the `profile` scope. You can create custom attributes, and these custom attributes can be added to `mapped_claims` and will display in the response.",
+	)
+
+	resp.Schema = schema.Schema{
 		// This description is used by the documentation generator and the language server.
 		Description: "Datasource to read PingOne OAuth 2.0 resource scope data.",
 
-		ReadContext: datasourcePingOneResourceScopeRead,
+		Attributes: map[string]schema.Attribute{
+			"id": framework.Attr_ID(),
 
-		Schema: map[string]*schema.Schema{
-			"environment_id": {
-				Description:      "The ID of the environment.",
-				Type:             schema.TypeString,
-				Required:         true,
-				ValidateDiagFunc: validation.ToDiagFunc(verify.ValidP1ResourceID),
-			},
-			"resource_id": {
-				Description:      "The ID of the resource that the scope belongs to.",
-				Type:             schema.TypeString,
-				Required:         true,
-				ValidateDiagFunc: validation.ToDiagFunc(verify.ValidP1ResourceID),
-			},
-			"resource_scope_id": {
-				Description:      "The ID of the resource scope.",
-				Type:             schema.TypeString,
-				Optional:         true,
-				ConflictsWith:    []string{"name"},
-				ValidateDiagFunc: validation.ToDiagFunc(verify.ValidP1ResourceID),
-			},
-			"name": {
-				Description:   "The name of the resource scope.",
-				Type:          schema.TypeString,
-				Optional:      true,
-				ConflictsWith: []string{"resource_scope_id"},
-			},
-			"description": {
-				Description: "A description of the resource scope.",
-				Type:        schema.TypeString,
-				Computed:    true,
-			},
-			"schema_attributes": {
-				Description: "A list that specifies the user schema attributes that can be read or updated for the specified PingOne access control scope. The value is an array of schema attribute paths (such as username, name.given, shirtSize) that the scope controls. This property is supported only for the `p1:read:user`, `p1:update:user` and `p1:read:user:{suffix}` and `p1:update:user:{suffix}` scopes. No other PingOne platform scopes allow this behavior. Any attributes not listed in the attribute array are excluded from the read or update action. The wildcard path (*) in the array includes all attributes and cannot be used in conjunction with any other user schema attribute path.",
-				Type:        schema.TypeList,
-				Computed:    true,
-				Elem: &schema.Schema{
-					Type: schema.TypeString,
+			"environment_id": framework.Attr_LinkID(
+				framework.SchemaAttributeDescriptionFromMarkdown("The ID of the environment that is configured with the resource scope."),
+			),
+
+			"resource_id": framework.Attr_LinkID(
+				framework.SchemaAttributeDescriptionFromMarkdown("The ID of the resource that the scope belongs to."),
+			),
+
+			"resource_scope_id": schema.StringAttribute{
+				Description:         resourceScopeIdDescription.Description,
+				MarkdownDescription: resourceScopeIdDescription.MarkdownDescription,
+				Optional:            true,
+				Computed:            true,
+				Validators: []validator.String{
+					stringvalidator.ExactlyOneOf(path.MatchRelative().AtParent().AtName("name")),
+					verify.P1ResourceIDValidator(),
 				},
 			},
-			"mapped_claims": {
-				Description: "A list of custom resource attribute IDs. This property applies only for the resource with its type property set to `OPENID_CONNECT`. Moreover, this property does not display predefined OpenID Connect (OIDC) mappings, such as the `email` claim in the OIDC `email` scope or the `name` claim in the `profile` scope. You can create custom attributes, and these custom attributes can be added to `mapped_claims` and will display in the response.",
-				Type:        schema.TypeList,
-				Computed:    true,
-				Elem: &schema.Schema{
-					Type: schema.TypeString,
+
+			"name": schema.StringAttribute{
+				Description:         nameDescription.Description,
+				MarkdownDescription: nameDescription.MarkdownDescription,
+				Optional:            true,
+				Computed:            true,
+				Validators: []validator.String{
+					stringvalidator.ExactlyOneOf(path.MatchRelative().AtParent().AtName("resource_scope_id")),
+					stringvalidator.LengthAtLeast(nameLength),
 				},
+			},
+
+			"description": schema.StringAttribute{
+				Description: framework.SchemaAttributeDescriptionFromMarkdown("A description of the resource scope.").Description,
+				Computed:    true,
+			},
+
+			"schema_attributes": schema.SetAttribute{
+				Description:         schemaAttributesDescription.Description,
+				MarkdownDescription: schemaAttributesDescription.MarkdownDescription,
+				Computed:            true,
+
+				ElementType: types.StringType,
+			},
+
+			"mapped_claims": schema.SetAttribute{
+				Description:         mappedClaimsDescription.Description,
+				MarkdownDescription: mappedClaimsDescription.MarkdownDescription,
+				Computed:            true,
+
+				ElementType: types.StringType,
 			},
 		},
 	}
 }
 
-func datasourcePingOneResourceScopeRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	p1Client := meta.(*client.Client)
-	apiClient := p1Client.API.ManagementAPIClient
+func (r *ResourceScopeDataSource) Configure(ctx context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
 
+	resourceConfig, ok := req.ProviderData.(framework.ResourceType)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Resource Configure Type",
+			fmt.Sprintf("Expected the provider client, got: %T. Please report this issue to the provider maintainers.", req.ProviderData),
+		)
+
+		return
+	}
+
+	preparedClient, err := PrepareClient(ctx, resourceConfig)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Client not initialized",
+			err.Error(),
+		)
+
+		return
+	}
+
+	r.Client = preparedClient
+}
+
+func (r *ResourceScopeDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var data *ResourceScopeDataSourceModel
+
+	if r.Client == nil {
+		resp.Diagnostics.AddError(
+			"Client not initialized",
+			"Expected the PingOne client, got nil.  Please report this issue to the provider maintainers.")
+		return
+	}
+
+	// Read Terraform prior state data into the model
+	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	var resourceScope *management.ResourceScope
+
+	if !data.Name.IsNull() {
+
+		var d diag.Diagnostics
+		resourceScope, d = fetchResourceScopeFromName(ctx, r.Client, data.EnvironmentId.ValueString(), data.ResourceId.ValueString(), data.Name.ValueString())
+		resp.Diagnostics.Append(d...)
+		if resp.Diagnostics.HasError() {
+			return
+		}
+
+	} else if !data.ResourceScopeId.IsNull() {
+
+		var d diag.Diagnostics
+		resourceScope, d = fetchResourceScopeFromID(ctx, r.Client, data.EnvironmentId.ValueString(), data.ResourceId.ValueString(), data.ResourceScopeId.ValueString())
+		resp.Diagnostics.Append(d...)
+		if resp.Diagnostics.HasError() {
+			return
+		}
+
+	} else {
+		resp.Diagnostics.AddError(
+			"Missing parameter",
+			"Cannot find the requested resource scope. resource_scope_id or name must be set.",
+		)
+		return
+	}
+
+	// Save updated data into Terraform state
+	resp.Diagnostics.Append(data.toState(resourceScope)...)
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (p *ResourceScopeDataSourceModel) toState(apiObject *management.ResourceScope) diag.Diagnostics {
 	var diags diag.Diagnostics
 
-	var resp management.ResourceScope
-
-	if v, ok := d.GetOk("name"); ok {
-
-		respList, diags := sdk.ParseResponse(
-			ctx,
-
-			func() (any, *http.Response, error) {
-				return apiClient.ResourceScopesApi.ReadAllResourceScopes(ctx, d.Get("environment_id").(string), d.Get("resource_id").(string)).Execute()
-			},
-			"ReadAllResourceScopes",
-			sdk.DefaultCustomError,
-			sdk.DefaultCreateReadRetryable,
+	if apiObject == nil {
+		diags.AddError(
+			"Data object missing",
+			"Cannot convert the data object to state as the data object is nil.  Please report this to the provider maintainers.",
 		)
-		if diags.HasError() {
-			return diags
-		}
-
-		if scopes, ok := respList.(*management.EntityArray).Embedded.GetScopesOk(); ok {
-
-			found := false
-			for _, scope := range scopes {
-
-				if scope.GetName() == v.(string) {
-					resp = scope
-					found = true
-					break
-				}
-			}
-
-			if !found {
-				diags = append(diags, diag.Diagnostic{
-					Severity: diag.Error,
-					Summary:  fmt.Sprintf("Cannot find resource scope %s", v),
-				})
-
-				return diags
-			}
-
-		}
-
-	} else if v, ok2 := d.GetOk("resource_scope_id"); ok2 {
-
-		resourceResp, diags := sdk.ParseResponse(
-			ctx,
-
-			func() (any, *http.Response, error) {
-				return apiClient.ResourceScopesApi.ReadOneResourceScope(ctx, d.Get("environment_id").(string), d.Get("resource_id").(string), v.(string)).Execute()
-			},
-			"ReadOneResourceScope",
-			sdk.DefaultCustomError,
-			sdk.DefaultCreateReadRetryable,
-		)
-		if diags.HasError() {
-			return diags
-		}
-
-		resp = *resourceResp.(*management.ResourceScope)
-
-	} else {
-
-		diags = append(diags, diag.Diagnostic{
-			Severity: diag.Error,
-			Summary:  "Neither resource_scope_id or name are set",
-			Detail:   "Neither resource_scope_id or name are set",
-		})
 
 		return diags
-
 	}
 
-	d.SetId(resp.GetId())
-	d.Set("resource_scope_id", resp.GetId())
-	d.Set("name", resp.GetName())
-
-	if v, ok := resp.GetDescriptionOk(); ok {
-		d.Set("description", v)
-	} else {
-		d.Set("description", nil)
-	}
-
-	if v, ok := resp.GetSchemaAttributesOk(); ok {
-		d.Set("schema_attributes", v)
-	} else {
-		d.Set("schema_attributes", nil)
-	}
-
-	if v, ok := resp.GetMappedClaimsOk(); ok {
-		d.Set("mapped_claims", v)
-	} else {
-		d.Set("mapped_claims", nil)
-	}
+	p.Id = framework.StringToTF(apiObject.GetId())
+	p.ResourceId = framework.StringToTF(apiObject.GetId())
+	p.ResourceScopeId = framework.StringToTF(apiObject.GetId())
+	p.Name = framework.StringOkToTF(apiObject.GetNameOk())
+	p.Description = framework.StringOkToTF(apiObject.GetDescriptionOk())
+	p.SchemaAttributes = framework.StringSetOkToTF(apiObject.GetSchemaAttributesOk())
+	p.MappedClaims = framework.StringSetOkToTF(apiObject.GetMappedClaimsOk())
 
 	return diags
 }
 
+func fetchResourceScopeFromID(ctx context.Context, apiClient *management.APIClient, environmentID, resourceID, resourceScopeID string) (*management.ResourceScope, diag.Diagnostics) {
+	var diags diag.Diagnostics
+
+	var resourceScope *management.ResourceScope
+	diags.Append(framework.ParseResponse(
+		ctx,
+
+		func() (any, *http.Response, error) {
+			return apiClient.ResourceScopesApi.ReadOneResourceScope(ctx, environmentID, resourceID, resourceScopeID).Execute()
+		},
+		"ReadOneResourceScope",
+		framework.DefaultCustomError,
+		sdk.DefaultCreateReadRetryable,
+		&resourceScope,
+	)...)
+	if diags.HasError() {
+		return nil, diags
+	}
+
+	return resourceScope, diags
+}
+
 func fetchResourceScopeFromName(ctx context.Context, apiClient *management.APIClient, environmentID, resourceID, resourceScopeName string) (*management.ResourceScope, diag.Diagnostics) {
+	var diags diag.Diagnostics
 
-	var resp *management.ResourceScope
+	resourceScopes, d := fetchResourceScopesFromNames(ctx, apiClient, environmentID, resourceID, []string{resourceScopeName})
+	diags.Append(d...)
+	if diags.HasError() {
+		return nil, diags
+	}
 
-	respList, diags := sdk.ParseResponse(
+	if len(resourceScopes) == 0 {
+		diags.AddError(
+			"Cannot find resource scope from ID",
+			fmt.Sprintf("The resource scope %s for resource %s in environment %s cannot be found", resourceScopeName, resourceID, environmentID),
+		)
+		return nil, diags
+	}
+
+	if len(resourceScopes) > 1 {
+		diags.AddError(
+			"Multiple resource scopes found from ID",
+			fmt.Sprintf("Multiple resource scopes %s for resource %s in environment %s were found", resourceScopeName, resourceID, environmentID),
+		)
+		return nil, diags
+	}
+
+	return &resourceScopes[0], diags
+}
+
+func fetchResourceScopesFromIDs(ctx context.Context, apiClient *management.APIClient, environmentID string, resourceID string, resourceScopeIDs []string) ([]management.ResourceScope, diag.Diagnostics) {
+	return fetchResourceScopesFromIDOrNameSlice(ctx, apiClient, environmentID, resourceID, resourceScopeIDs, false)
+}
+
+func fetchResourceScopesFromNames(ctx context.Context, apiClient *management.APIClient, environmentID string, resourceID string, resourceScopeNames []string) ([]management.ResourceScope, diag.Diagnostics) {
+	return fetchResourceScopesFromIDOrNameSlice(ctx, apiClient, environmentID, resourceID, resourceScopeNames, true)
+}
+
+func fetchResourceScopesFromIDOrNameSlice(ctx context.Context, apiClient *management.APIClient, environmentID, resourceID string, resourceScopeInputList []string, byName bool) ([]management.ResourceScope, diag.Diagnostics) {
+	var diags diag.Diagnostics
+
+	var entityArray *management.EntityArray
+	diags.Append(framework.ParseResponse(
 		ctx,
 
 		func() (any, *http.Response, error) {
 			return apiClient.ResourceScopesApi.ReadAllResourceScopes(ctx, environmentID, resourceID).Execute()
 		},
 		"ReadAllResourceScopes",
-		sdk.DefaultCustomError,
+		framework.DefaultCustomError,
 		sdk.DefaultCreateReadRetryable,
-	)
+		&entityArray,
+	)...)
 	if diags.HasError() {
 		return nil, diags
 	}
 
-	if resourceScopes, ok := respList.(*management.EntityArray).Embedded.GetScopesOk(); ok {
+	var foundResourceScopes []management.ResourceScope
 
-		found := false
+	if resourceScopes, ok := entityArray.Embedded.GetScopesOk(); ok {
+		resourceScopesMap := make(map[string]management.ResourceScope)
+
 		for _, resourceScope := range resourceScopes {
-
-			resourceScope := resourceScope // fix for exportloopref lint
-
-			if resourceScope.GetName() == resourceScopeName {
-				resp = &resourceScope
-				found = true
-				break
+			if byName {
+				resourceScopesMap[strings.ToLower(resourceScope.GetName())] = resourceScope
+			} else {
+				resourceScopesMap[strings.ToLower(resourceScope.GetId())] = resourceScope
 			}
 		}
 
-		if !found {
-			diags = append(diags, diag.Diagnostic{
-				Severity: diag.Error,
-				Summary:  fmt.Sprintf("Cannot find resource %s", resourceScopeName),
-			})
-
-			return nil, diags
+		for _, resourceScopeInputItem := range resourceScopeInputList {
+			if v, ok := resourceScopesMap[strings.ToLower(resourceScopeInputItem)]; ok {
+				foundResourceScopes = append(foundResourceScopes, v)
+			} else {
+				diags.AddError(
+					"Cannot find resource scope",
+					fmt.Sprintf("The resource scope %s for resource %s in environment %s cannot be found", resourceScopeInputItem, resourceID, environmentID),
+				)
+			}
 		}
 
+		if diags.HasError() {
+			return nil, diags
+		}
 	}
 
-	return resp, diags
+	return foundResourceScopes, diags
 }
