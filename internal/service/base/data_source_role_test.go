a963b2e0a402971339ed89eb640ae89becf7035a
Patrick Cowland <44225864+patrickcping@users.noreply.github.com>
2023-10-17 18:04:05 +0100

    `data-source/pingone_role`: Migrate to plugin framework (#592)
    * `data-source/pingone_role`: Migrated to plugin framework.
    
    * update documentation
    
    * changelog
    
    * update test for new input validation

--- internal/service/base/data_source_role_test.go
+++ internal/service/base/data_source_role_test.go
@@ -107,7 +107,7 @@ func TestAccRoleDataSource_NotFound(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config:      testAccRoleDataSourceConfig_NotFoundByName(resourceName),
-				ExpectError: regexp.MustCompile("Cannot find role doesnotexist"),
+				ExpectError: regexp.MustCompile("Invalid Attribute Value Match"),
 			},
 			// {
 			// 	Config:      testAccRoleDataSourceConfig_NotFoundByID(resourceName),
