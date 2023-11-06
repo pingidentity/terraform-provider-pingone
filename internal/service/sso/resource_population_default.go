9da48860467c29922af416d98bc1779d490720bb
Patrick Cowland <patrickcowland@pingidentity.com>
2023-11-06 15:38:52 +0000

    update `pingone_population_default` timeout description

--- internal/service/sso/resource_population_default.go
+++ internal/service/sso/resource_population_default.go
@@ -95,7 +95,7 @@ func (r *PopulationDefaultResource) Schema(ctx context.Context, req resource.Sch
 
 			"timeouts": timeouts.Attributes(ctx, timeouts.Opts{
 				Create:            true,
-				CreateDescription: "A timeout to apply to creation of the resource.  There may be a short delay in provisioning this resource when creating the parent PingOne environment at the same time (referenced by the `environment_id` parameter), as the platform will create a default population automatically.  This resource will attempt to find and update the existing default population, and will wait if the default population cannot be found (for example, if it is in the process of being created automatically by the platform).  This timeout value can be used to override the wait time, and force the creation of a default population.  The default value is 20 minutes.",
+				CreateDescription: "A timeout to apply to creation of the resource.  There may be a short delay in provisioning this resource when creating the parent PingOne environment at the same time (referenced by the `environment_id` parameter), as the platform will create a default population automatically.  This resource will attempt to find and update the existing default population, and will wait if the default population cannot be found (for example, if it is in the process of being created automatically by the platform).  This timeout value can be used to override the wait time, and force the creation of a default population.  The value is expected to be a string that can be parsed as a duration consisting of numbers and unit suffixes, such as `30s` or `2h45m`. Valid time units are `s` (seconds), `m` (minutes), `h` (hours).  The default value is `20m` (20 minutes).",
 			}),
 		},
 	}
