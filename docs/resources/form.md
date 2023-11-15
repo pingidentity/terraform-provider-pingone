07306640634064d4e4cf6e282b0fff4fe38b0c2a
Patrick Cowland <patrickcowland@pingidentity.com>
2023-11-15 10:48:51 +0000

    doc example and draft of test

--- docs/resources/form.md
+++ docs/resources/form.md
@@ -18,6 +18,26 @@ resource "pingone_environment" "my_environment" {
 
 resource "pingone_form" "my_awesome_form" {
   environment_id = pingone_environment.my_environment.id
+
+  name        = "My Awesome Form"
+  description = "This is my awesome form"
+
+  mark_required = false
+  mark_optional = true
+
+  cols = 4
+
+  language_bundle = {
+    "button.text"                              = "Submit",
+    "fields.user.email.label"                  = "Email Address",
+    "fields.user.password.label"               = "Password"
+    "fields.user.password.labelPasswordVerify" = "Verify Password",
+    "fields.user.username.label"               = "Username",
+  }
+
+  components = {
+    fields = [{}]
+  }
 }
 ```
 
--- internal/service/base/resource_form_test.go
+++ internal/service/base/resource_form_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/pingidentity/terraform-provider-pingone/internal/acctest"
+	"github.com/pingidentity/terraform-provider-pingone/internal/acctest/service/authorize"
 	"github.com/pingidentity/terraform-provider-pingone/internal/acctest/service/base"
 	client "github.com/pingidentity/terraform-provider-pingone/internal/client"
 	"github.com/pingidentity/terraform-provider-pingone/internal/verify"
@@ -46,7 +47,7 @@ func TestAccForm_RemovalDrift(t *testing.T) {
 		Steps: []resource.TestStep{
 			// Configure
 			{
-				Config: testAccFormConfig_Minimal(environmentName, licenseID, resourceName, name),
+				Config: testAccFormConfig_NewEnv(environmentName, licenseID, resourceName, name),
 				Check:  base.Form_GetIDs(resourceFullName, &environmentID, &formID),
 			},
 			{
@@ -58,7 +59,7 @@ func TestAccForm_RemovalDrift(t *testing.T) {
 			},
 			// Test removal of the environment
 			{
-				Config: testAccFormConfig_Minimal(environmentName, licenseID, resourceName, name),
+				Config: testAccFormConfig_NewEnv(environmentName, licenseID, resourceName, name),
 				Check:  base.Form_GetIDs(resourceFullName, &environmentID, &formID),
 			},
 			{
@@ -71,11 +72,12 @@ func TestAccForm_RemovalDrift(t *testing.T) {
 		},
 	})
 }
-func TestAccForm_BadParameters(t *testing.T) {
+
+func TestAccForm_NewEnv(t *testing.T) {
 	t.Parallel()
 
 	resourceName := acctest.ResourceNameGen()
-	resourceFullName := fmt.Sprintf("pingone_form.%s", resourceName)
+	resourceFullName := fmt.Sprintf("pingone_authorize_decision_endpoint.%s", resourceName)
 
 	environmentName := acctest.ResourceNameGenEnvironment()
 
@@ -90,12 +92,125 @@ func TestAccForm_BadParameters(t *testing.T) {
 			acctest.PreCheckNoFeatureFlag(t)
 		},
 		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
+		CheckDestroy:             authorize.DecisionEndpoint_CheckDestroy,
+		ErrorCheck:               acctest.ErrorCheck(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccFormConfig_NewEnv(environmentName, licenseID, resourceName, name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceFullName, "name", name),
+				),
+			},
+		},
+	})
+}
+
+func TestAccForm_Full(t *testing.T) {
+	t.Parallel()
+
+	resourceName := acctest.ResourceNameGen()
+	resourceFullName := fmt.Sprintf("pingone_form.%s", resourceName)
+
+	name := resourceName
+
+	fullStep := resource.TestStep{
+		Config: testAccFormConfig_Full(resourceName, name),
+		Check: resource.ComposeTestCheckFunc(
+			resource.TestMatchResourceAttr(resourceFullName, "id", verify.P1ResourceIDRegexpFullString),
+			resource.TestMatchResourceAttr(resourceFullName, "environment_id", verify.P1ResourceIDRegexpFullString),
+			resource.TestCheckResourceAttr(resourceFullName, "name", name),
+			resource.TestCheckResourceAttr(resourceFullName, "description", "This is my awesome form"),
+			resource.TestCheckResourceAttr(resourceFullName, "category", "CUSTOM"),
+			resource.TestCheckResourceAttr(resourceFullName, "mark_required", "false"),
+			resource.TestCheckResourceAttr(resourceFullName, "mark_optional", "true"),
+			resource.TestCheckResourceAttr(resourceFullName, "cols", "4"),
+			resource.TestCheckResourceAttr(resourceFullName, "language_bundle", "test"),
+			resource.TestCheckResourceAttr(resourceFullName, "translation_method", "DEFAULT_VALUE"),
+		),
+	}
+
+	minimalStep := resource.TestStep{
+		Config: testAccFormConfig_Minimal(resourceName, name),
+		Check: resource.ComposeTestCheckFunc(
+			resource.TestMatchResourceAttr(resourceFullName, "id", verify.P1ResourceIDRegexpFullString),
+			resource.TestMatchResourceAttr(resourceFullName, "environment_id", verify.P1ResourceIDRegexpFullString),
+			resource.TestCheckResourceAttr(resourceFullName, "name", name),
+			resource.TestCheckNoResourceAttr(resourceFullName, "description"),
+			resource.TestCheckResourceAttr(resourceFullName, "category", "CUSTOM"),
+			resource.TestCheckResourceAttr(resourceFullName, "mark_required", "true"),
+			resource.TestCheckResourceAttr(resourceFullName, "mark_optional", "false"),
+			resource.TestCheckNoResourceAttr(resourceFullName, "cols"),
+			resource.TestCheckNoResourceAttr(resourceFullName, "language_bundle"),
+			resource.TestCheckNoResourceAttr(resourceFullName, "translation_method"),
+		),
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheckClient(t)
+			acctest.PreCheckNoFeatureFlag(t)
+		},
+		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
+		CheckDestroy:             base.Form_CheckDestroy,
+		ErrorCheck:               acctest.ErrorCheck(t),
+		Steps: []resource.TestStep{
+			// Full step
+			fullStep,
+			{
+				Config:  testAccFormConfig_Full(resourceName, name),
+				Destroy: true,
+			},
+			// Minimal step
+			minimalStep,
+			{
+				Config:  testAccFormConfig_Minimal(resourceName, name),
+				Destroy: true,
+			},
+			// Change
+			fullStep,
+			minimalStep,
+			fullStep,
+			// Test importing the resource
+			{
+				ResourceName: resourceFullName,
+				ImportStateIdFunc: func() resource.ImportStateIdFunc {
+					return func(s *terraform.State) (string, error) {
+						rs, ok := s.RootModule().Resources[resourceFullName]
+						if !ok {
+							return "", fmt.Errorf("Resource Not found: %s", resourceFullName)
+						}
+
+						return fmt.Sprintf("%s/%s", rs.Primary.Attributes["environment_id"], rs.Primary.ID), nil
+					}
+				}(),
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccForm_BadParameters(t *testing.T) {
+	t.Parallel()
+
+	resourceName := acctest.ResourceNameGen()
+	resourceFullName := fmt.Sprintf("pingone_form.%s", resourceName)
+
+	name := resourceName
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheckClient(t)
+			acctest.PreCheckNewEnvironment(t)
+			acctest.PreCheckNoFeatureFlag(t)
+		},
+		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
 		CheckDestroy:             base.Form_CheckDestroy,
 		ErrorCheck:               acctest.ErrorCheck(t),
 		Steps: []resource.TestStep{
 			// Configure
 			{
-				Config: testAccFormConfig_Minimal(environmentName, licenseID, resourceName, name),
+				Config: testAccFormConfig_Minimal(resourceName, name),
 			},
 			// Errors
 			{
@@ -119,3 +234,71 @@ func TestAccForm_BadParameters(t *testing.T) {
 	})
 }
 
+func testAccFormConfig_NewEnv(environmentName, licenseID, resourceName, name string) string {
+	return fmt.Sprintf(`
+	%[1]s
+
+resource "pingone_form" "%[3]s" {
+  environment_id = pingone_environment.%[2]s.id
+
+  name = "My Awesome Minimal Form"
+
+  mark_required = true
+  mark_optional = false
+
+  components = {
+    fields = [{}]
+  }
+}`, acctest.MinimalSandboxEnvironment(environmentName, licenseID), environmentName, resourceName, name)
+}
+
+func testAccFormConfig_Full(resourceName, name string) string {
+	return fmt.Sprintf(`
+		%[1]s
+
+resource "pingone_form" "%[2]s" {
+  environment_id = data.pingone_environment.general_test.id
+
+  name        = "%[3]s"
+  description = "This is my awesome form"
+
+  category = "CUSTOM"
+
+  mark_required = false
+  mark_optional = true
+
+  cols = 4
+
+  language_bundle = {
+    "button.text"                              = "Submit",
+    "fields.user.email.label"                  = "Email Address",
+    "fields.user.password.label"               = "Password"
+    "fields.user.password.labelPasswordVerify" = "Verify Password",
+    "fields.user.username.label"               = "Username",
+  }
+
+  translation_method = "DEFAULT_VALUE"
+
+  components = {
+    fields = [{}]
+  }
+}`, acctest.GenericSandboxEnvironment(), resourceName, name)
+}
+
+func testAccFormConfig_Minimal(resourceName, name string) string {
+	return fmt.Sprintf(`
+	%[1]s
+
+resource "pingone_form" "%[2]s" {
+  environment_id = data.pingone_environment.general_test.id
+
+  name        = "%[3]s"
+
+  mark_required = true
+  mark_optional = false
+
+  components = {
+    fields = [{}]
+  }
+}`, acctest.GenericSandboxEnvironment(), resourceName, name)
+}
