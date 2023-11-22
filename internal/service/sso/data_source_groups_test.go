62fae4eda6f8aeae04010f4d1d89a5a320a73e7a
Patrick Cowland <patrickcowland@pingidentity.com>
2023-11-22 10:46:33 +0000

    New data source `pingone_groups`

---
+++ internal/service/sso/data_source_groups_test.go
@@ -0,0 +1,228 @@
+package sso_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/pingidentity/terraform-provider-pingone/internal/acctest"
+	"github.com/pingidentity/terraform-provider-pingone/internal/acctest/service/sso"
+	"github.com/pingidentity/terraform-provider-pingone/internal/verify"
+)
+
+func TestAccGroupsDataSource_BySCIMFilter(t *testing.T) {
+	t.Parallel()
+
+	resourceName := acctest.ResourceNameGen()
+	dataSourceFullName := fmt.Sprintf("data.pingone_groups.%s", resourceName)
+
+	name := resourceName
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheckClient(t)
+			acctest.PreCheckNoFeatureFlag(t)
+		},
+		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
+		CheckDestroy:             sso.Group_CheckDestroy,
+		ErrorCheck:               acctest.ErrorCheck(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccGroupsDataSourceConfig_BySCIMFilter(resourceName, fmt.Sprintf(`(name eq \"%s-1\") OR (name eq \"%s-2\")`, name, name), name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestMatchResourceAttr(dataSourceFullName, "id", verify.P1ResourceIDRegexpFullString),
+					resource.TestMatchResourceAttr(dataSourceFullName, "environment_id", verify.P1ResourceIDRegexpFullString),
+					resource.TestCheckResourceAttr(dataSourceFullName, "ids.#", "2"),
+					resource.TestMatchResourceAttr(dataSourceFullName, "ids.0", verify.P1ResourceIDRegexpFullString),
+					resource.TestMatchResourceAttr(dataSourceFullName, "ids.1", verify.P1ResourceIDRegexpFullString),
+				),
+			},
+		},
+	})
+}
+
+func TestAccGroupsDataSource_ByDataFilter(t *testing.T) {
+	t.Parallel()
+
+	resourceName := acctest.ResourceNameGen()
+	dataSourceFullName := fmt.Sprintf("data.pingone_groups.%s", resourceName)
+
+	name := resourceName
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheckClient(t)
+			acctest.PreCheckNoFeatureFlag(t)
+		},
+		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
+		CheckDestroy:             sso.Group_CheckDestroy,
+		ErrorCheck:               acctest.ErrorCheck(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccGroupsDataSourceConfig_ByDataFilter1(resourceName, name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestMatchResourceAttr(dataSourceFullName, "id", verify.P1ResourceIDRegexpFullString),
+					resource.TestMatchResourceAttr(dataSourceFullName, "environment_id", verify.P1ResourceIDRegexpFullString),
+					resource.TestCheckResourceAttr(dataSourceFullName, "ids.#", "2"),
+					resource.TestMatchResourceAttr(dataSourceFullName, "ids.0", verify.P1ResourceIDRegexpFullString),
+					resource.TestMatchResourceAttr(dataSourceFullName, "ids.1", verify.P1ResourceIDRegexpFullString),
+				),
+			},
+			{
+				Config: testAccGroupsDataSourceConfig_ByDataFilter2(resourceName, name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestMatchResourceAttr(dataSourceFullName, "id", verify.P1ResourceIDRegexpFullString),
+					resource.TestMatchResourceAttr(dataSourceFullName, "environment_id", verify.P1ResourceIDRegexpFullString),
+					resource.TestCheckResourceAttr(dataSourceFullName, "ids.#", "3"),
+					resource.TestMatchResourceAttr(dataSourceFullName, "ids.0", verify.P1ResourceIDRegexpFullString),
+					resource.TestMatchResourceAttr(dataSourceFullName, "ids.1", verify.P1ResourceIDRegexpFullString),
+					resource.TestMatchResourceAttr(dataSourceFullName, "ids.2", verify.P1ResourceIDRegexpFullString),
+				),
+			},
+		},
+	})
+}
+
+func TestAccGroupsDataSource_NotFound(t *testing.T) {
+	t.Parallel()
+
+	resourceName := acctest.ResourceNameGen()
+	dataSourceFullName := fmt.Sprintf("data.pingone_groups.%s", resourceName)
+
+	name := resourceName
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheckClient(t)
+			acctest.PreCheckNoFeatureFlag(t)
+		},
+		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
+		CheckDestroy:             sso.Group_CheckDestroy,
+		ErrorCheck:               acctest.ErrorCheck(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccGroupsDataSourceConfig_NotFound(resourceName, fmt.Sprintf(`(name eq \"%s-1\") OR (name eq \"%s-2\")`, name, name)),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestMatchResourceAttr(dataSourceFullName, "id", verify.P1ResourceIDRegexpFullString),
+					resource.TestMatchResourceAttr(dataSourceFullName, "environment_id", verify.P1ResourceIDRegexpFullString),
+					resource.TestCheckResourceAttr(dataSourceFullName, "ids.#", "0"),
+				),
+			},
+		},
+	})
+}
+
+func testAccGroupsDataSourceConfig_BySCIMFilter(resourceName, filter, name string) string {
+	return fmt.Sprintf(`
+	%[1]s
+
+resource "pingone_group" "%[2]s-1" {
+  environment_id = data.pingone_environment.general_test.id
+  name           = "%[3]s-1"
+}
+
+resource "pingone_group" "%[2]s-2" {
+  environment_id = data.pingone_environment.general_test.id
+  name           = "%[3]s-2"
+}
+
+resource "pingone_group" "%[2]s-3" {
+  environment_id = data.pingone_environment.general_test.id
+  name           = "%[3]s-3"
+}
+
+data "pingone_groups" "%[2]s" {
+  environment_id = data.pingone_environment.general_test.id
+
+  scim_filter = "%[4]s"
+
+  depends_on = [
+    pingone_group.%[2]s-1,
+    pingone_group.%[2]s-2,
+    pingone_group.%[2]s-3,
+  ]
+}
+`, acctest.GenericSandboxEnvironment(), resourceName, name, filter)
+}
+
+func testAccGroupsDataSourceConfig_ByDataFilter1(resourceName, name string) string {
+	return fmt.Sprintf(`
+	%[1]s
+
+resource "pingone_group" "%[2]s-1" {
+  environment_id = data.pingone_environment.general_test.id
+  name           = "%[3]s-1"
+}
+
+resource "pingone_group" "%[2]s-2" {
+  environment_id = data.pingone_environment.general_test.id
+  name           = "%[3]s-2"
+}
+
+resource "pingone_group" "%[2]s-3" {
+  environment_id = data.pingone_environment.general_test.id
+  name           = "%[3]s-3"
+}
+
+data "pingone_groups" "%[2]s" {
+  environment_id = data.pingone_environment.general_test.id
+
+  data_filter {
+    name   = "name"
+    values = ["%[3]s-1", "%[3]s-2"]
+  }
+
+  depends_on = [
+    pingone_group.%[2]s-1,
+    pingone_group.%[2]s-2,
+    pingone_group.%[2]s-3,
+  ]
+}`, acctest.GenericSandboxEnvironment(), resourceName, name)
+}
+
+func testAccGroupsDataSourceConfig_ByDataFilter2(resourceName, name string) string {
+	return fmt.Sprintf(`
+	%[1]s
+
+resource "pingone_group" "%[2]s-1" {
+  environment_id = data.pingone_environment.general_test.id
+  name           = "%[3]s-1"
+}
+
+resource "pingone_group" "%[2]s-2" {
+  environment_id = data.pingone_environment.general_test.id
+  name           = "%[3]s-2"
+}
+
+resource "pingone_group" "%[2]s-3" {
+  environment_id = data.pingone_environment.general_test.id
+  name           = "%[3]s-3"
+}
+
+data "pingone_groups" "%[2]s" {
+  environment_id = data.pingone_environment.general_test.id
+
+  data_filter {
+    name   = "name"
+    values = ["%[3]s-1", "%[3]s-2", "%[3]s-3", ]
+  }
+
+  depends_on = [
+    pingone_group.%[2]s-1,
+    pingone_group.%[2]s-2,
+    pingone_group.%[2]s-3,
+  ]
+}`, acctest.GenericSandboxEnvironment(), resourceName, name)
+}
+
+func testAccGroupsDataSourceConfig_NotFound(resourceName, filter string) string {
+	return fmt.Sprintf(`
+	%[1]s
+
+data "pingone_groups" "%[2]s" {
+  environment_id = data.pingone_environment.general_test.id
+
+  scim_filter = "%[3]s"
+}
+`, acctest.GenericSandboxEnvironment(), resourceName, filter)
+}
