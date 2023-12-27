3227ae6f38f06cdc345d982942bbe918dcfd7753
Patrick Cowland <44225864+patrickcping@users.noreply.github.com>
2023-12-27 17:49:16 +0000

    Support for DaVinci Form Builder (#655)
    * initial commit (pre-schema)
    
    * changelog
    
    * schema (1)
    
    * doc example and draft of test
    
    * schema (2)
    
    * schema (3) + resource
    
    * docs
    
    * test drafts (2)
    
    * updates for testing
    
    * dev and test updates
    
    * fields text+password completed
    
    * field "password verify" completed
    
    * field radio completed
    
    * field checkbox completed
    
    * field dropdown completed
    
    * completed divider item
    
    * completed "empty field" item
    
    * completed "error display" item
    
    * completed textblob item
    
    * completed "slate textblob" item
    
    * completed combobox field
    
    * completed "submit button" field
    
    * flow button in progress
    
    * completed "flow button" item
    
    * completed "flow link" item
    
    * completed recaptchav2 item
    
    * completed "qr code" item
    
    * lint
    
    * docs and multiple field test
    
    * lint
    
    * documentation and validation updates
    
    * Validation and documentation edits
    
    * New resource `pingone_forms_recaptcha_v2`

---
+++ internal/acctest/service/base/forms_recaptcha_v2.go
@@ -0,0 +1,80 @@
+package base
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/patrickcping/pingone-go-sdk-v2/management"
+	"github.com/pingidentity/terraform-provider-pingone/internal/acctest"
+)
+
+func FormsRecaptchaV2_CheckDestroy(s *terraform.State) error {
+	var ctx = context.Background()
+
+	p1Client, err := acctest.TestClient(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	apiClient := p1Client.API.ManagementAPIClient
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "pingone_forms_recaptcha_v2" {
+			continue
+		}
+
+		shouldContinue, err := acctest.CheckParentEnvironmentDestroy(ctx, apiClient, rs.Primary.Attributes["environment_id"])
+		if err != nil {
+			return err
+		}
+
+		if shouldContinue {
+			continue
+		}
+
+		_, r, err := apiClient.RecaptchaConfigurationApi.ReadRecaptchaConfiguration(ctx, rs.Primary.Attributes["environment_id"]).Execute()
+
+		destroyHttpCode := 204
+		shouldContinue, err = acctest.CheckForResourceDestroyCustomHTTPCode(r, err, destroyHttpCode)
+		if err != nil {
+			return err
+		}
+
+		if shouldContinue {
+			continue
+		}
+
+		return fmt.Errorf("PingOne Forms Recaptcha v2 config Instance %s still exists", rs.Primary.Attributes["environment_id"])
+	}
+
+	return nil
+}
+
+func FormsRecaptchaV2_GetIDs(resourceName string, environmentID *string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+
+		rs, ok := s.RootModule().Resources[resourceName]
+		if !ok {
+			return fmt.Errorf("Resource Not found: %s", resourceName)
+		}
+
+		*environmentID = rs.Primary.Attributes["environment_id"]
+
+		return nil
+	}
+}
+
+func FormsRecaptchaV2_RemovalDrift_PreConfig(ctx context.Context, apiClient *management.APIClient, t *testing.T, environmentID string) {
+	if environmentID == "" {
+		t.Fatalf("One of environment ID or form ID cannot be determined. Environment ID: %s", environmentID)
+	}
+
+	_, err := apiClient.RecaptchaConfigurationApi.DeleteRecaptchaConfiguration(ctx, environmentID).Execute()
+	if err != nil {
+		t.Fatalf("Failed to delete FormsRecaptchaV2: %v", err)
+	}
+}
