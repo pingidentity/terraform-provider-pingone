3d481215b6e6904e26a9df54d3fbb36c9dd30fbd
Patrick Cowland <44225864+patrickcping@users.noreply.github.com>
2023-12-27 17:40:08 +0000

    Bump modules 2023-12-27 (#701)
    * Bump modules 2023-12-27
    
    * changelog

--- internal/service/sso/data_source_application.go
+++ internal/service/sso/data_source_application.go
@@ -982,7 +982,7 @@ func (p *applicationDataSourceModel) accessControlOkToTF(apiObject *management.A
 		}
 
 		groupObj := map[string]attr.Value{
-			"type":   framework.StringOkToTF(group.GetTypeOk()),
+			"type":   framework.EnumOkToTF(group.GetTypeOk()),
 			"groups": framework.StringSetToTF(groups),
 		}
 
