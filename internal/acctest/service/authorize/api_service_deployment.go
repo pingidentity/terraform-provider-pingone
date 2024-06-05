52401dbd1a77319b1e328ede74c2c0bd7b262b01
Patrick Cowland <patrickcowland@pingidentity.com>
2024-06-05 19:30:33 +0100

    updates following testing

---
+++ internal/acctest/service/authorize/api_service_deployment.go
@@ -0,0 +1,27 @@
+package authorize
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+)
+
+func APIServiceDeployment_CheckDestroy(s *terraform.State) error {
+	return nil
+}
+
+func APIServiceDeployment_GetIDs(resourceName string, environmentID, resourceID *string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+
+		rs, ok := s.RootModule().Resources[resourceName]
+		if !ok {
+			return fmt.Errorf("Resource Not found: %s", resourceName)
+		}
+
+		*environmentID = rs.Primary.Attributes["environment_id"]
+		*resourceID = rs.Primary.Attributes["api_service_id"]
+
+		return nil
+	}
+}
