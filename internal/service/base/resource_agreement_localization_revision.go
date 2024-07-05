71e49cb722c2dadb35cf9a9fc5304b77fa8fa663
Patrick Cowland <44225864+patrickcping@users.noreply.github.com>
2024-07-05 18:07:49 +0100

    `resource/pingone_agreement_localization_revision`: Add buffer time to the revision effective date to avoid blocking error (#883)
    * `resource/pingone_agreement_localization_revision`: Add buffer time to the revision effective date to avoid blocking error
    
    * correct lint issue
    
    * changelog

--- internal/service/base/resource_agreement_localization_revision.go
+++ internal/service/base/resource_agreement_localization_revision.go
@@ -430,7 +430,8 @@ func (p *AgreementLocalizationRevisionResourceModel) expand() (*management.Agree
 		t, d = p.EffectiveAt.ValueRFC3339Time()
 		diags.Append(d...)
 	} else {
-		t = time.Now().Local()
+		bufferTimeMins := 10 * time.Minute
+		t = time.Now().Local().Add(bufferTimeMins)
 	}
 	if diags.HasError() {
 		return nil, diags
