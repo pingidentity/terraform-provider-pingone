6586e41a2797ef609bc7102148e50d92ff9e71f0
henryrecker-pingidentity <58565376+henryrecker-pingidentity@users.noreply.github.com>
2025-08-06 14:31:03 -0500

    Initial davinci_connector_instances data source and test

--- internal/service/davinci/service.go
+++ internal/service/davinci/service.go
@@ -26,5 +26,6 @@ func Resources() []func() resource.Resource {
 func DataSources() []func() datasource.DataSource {
 	return []func() datasource.DataSource{
 		NewDavinciConnectorInstanceDataSource,
+		NewDavinciConnectorInstancesDataSource,
 	}
 }
