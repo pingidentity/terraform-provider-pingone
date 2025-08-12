218a4a35ce285839bfb2e5c33bef67ff906570d6
henryrecker-pingidentity <58565376+henryrecker-pingidentity@users.noreply.github.com>
2025-08-12 14:25:39 -0500

    Add missing test util method for new SDK

--- internal/acctest/service/base/environment.go
+++ internal/acctest/service/base/environment.go
@@ -11,14 +11,50 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"github.com/pingidentity/pingone-go-client/pingone"
+	"github.com/pingidentity/terraform-provider-pingone/internal/acctest"
 )
+
+func Environment_CheckDestroy(s *terraform.State) error {
+	var ctx = context.Background()
+
+	p1Client, err := acctest.TestClient(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "pingone_environment" {
+			continue
+		}
+
+		environmentIdUuid, err := uuid.Parse(rs.Primary.ID)
+		if err != nil {
+			return fmt.Errorf("unable to parse environment id '%s' as uuid: %v", environmentIdUuid, err)
+		}
+
+		_, r, err := p1Client.EnvironmentsApi.GetEnvironmentById(ctx, environmentIdUuid).Execute()
+
+		if r.StatusCode == 404 {
+			continue
+		}
+
+		if err != nil {
+			return err
+		}
+
+		return fmt.Errorf("PingOne Environment Instance %s still exists", rs.Primary.ID)
+	}
+
+	return nil
+}
 
 func Environment_GetIDs(resourceName string, resourceID *string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 
 		rs, ok := s.RootModule().Resources[resourceName]
 		if !ok {
-			return fmt.Errorf("Resource Not found: %s", resourceName)
+			return fmt.Errorf("resource Not found: %s", resourceName)
 		}
 
 		if resourceID != nil {
