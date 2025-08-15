2a71552058337e1df0c78e036705543986375eac
henryrecker-pingidentity <58565376+henryrecker-pingidentity@users.noreply.github.com>
2025-08-15 11:33:09 -0500

    Update TestAccLicenseDataSource_ByIDFull for SG
    Differences in SG license capabilities have been addressed, so the SG-specific checks can be removed

--- internal/service/base/data_source_license_test.go
+++ internal/service/base/data_source_license_test.go
@@ -27,13 +27,9 @@ func TestAccLicenseDataSource_ByIDFull(t *testing.T) {
 	positiveIntegerRegex := regexp.MustCompile(`^[1-9][0-9]*$`)
 
 	terminatesAtCheck := resource.TestCheckNoResourceAttr(dataSourceFullName, "terminates_at")
-	allowDataConsentCheck := resource.TestCheckResourceAttr(dataSourceFullName, "intelligence.allow_data_consent", "true")
-	allowAdvancedPredictorsCheck := resource.TestCheckResourceAttr(dataSourceFullName, "intelligence.allow_advanced_predictors", "true")
 	if os.Getenv("PINGONE_REGION_CODE") == "SG" {
-		// The SG license has a few differences
+		// The SG license has a terminates_at field
 		terminatesAtCheck = resource.TestMatchResourceAttr(dataSourceFullName, "terminates_at", verify.RFC3339Regexp)
-		allowDataConsentCheck = resource.TestCheckResourceAttr(dataSourceFullName, "intelligence.allow_data_consent", "false")
-		allowAdvancedPredictorsCheck = resource.TestCheckResourceAttr(dataSourceFullName, "intelligence.allow_advanced_predictors", "false")
 	}
 
 	resource.Test(t, resource.TestCase{
@@ -92,9 +88,9 @@ func TestAccLicenseDataSource_ByIDFull(t *testing.T) {
 					resource.TestCheckResourceAttr(dataSourceFullName, "intelligence.allow_geo_velocity", "true"),
 					resource.TestCheckResourceAttr(dataSourceFullName, "intelligence.allow_anonymous_network_detection", "true"),
 					resource.TestCheckResourceAttr(dataSourceFullName, "intelligence.allow_reputation", "true"),
-					allowDataConsentCheck,
+					resource.TestCheckResourceAttr(dataSourceFullName, "intelligence.allow_data_consent", "true"),
 					resource.TestCheckResourceAttr(dataSourceFullName, "intelligence.allow_risk", "true"),
-					allowAdvancedPredictorsCheck,
+					resource.TestCheckResourceAttr(dataSourceFullName, "intelligence.allow_advanced_predictors", "true"),
 					resource.TestCheckResourceAttr(dataSourceFullName, "mfa.allow_push_notification", "true"),
 					resource.TestCheckResourceAttr(dataSourceFullName, "mfa.allow_notification_outside_whitelist", "true"),
 					resource.TestCheckResourceAttr(dataSourceFullName, "mfa.allow_fido2_devices", "true"),
