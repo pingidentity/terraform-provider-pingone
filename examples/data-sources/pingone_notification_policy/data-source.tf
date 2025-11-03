d4b1e88f680997cee061b3b59f8fc301b9bf0fc4
jonathanfuller-pingidentity <jonathanfuller@pingidentity.com>
2025-11-03 12:37:33 -0600

    Data source documentation

---
+++ examples/data-sources/pingone_notification_policy/data-source.tf
@@ -0,0 +1,11 @@
+data "pingone_notification_policy" "example_by_name" {
+  environment_id = var.environment_id
+
+  name = "My notification policy"
+}
+
+data "pingone_notification_policy" "example_by_id" {
+  environment_id = var.environment_id
+
+  notification_policy_id = var.notification_policy_id
+}
