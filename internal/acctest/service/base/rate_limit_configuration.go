ab7855b4b3aa711594ea2d44b4acd1b1cae67047
jonathanfuller-pingidentity <jonathanfuller@pingidentity.com>
2025-12-04 11:15:44 -0600

    Add tests

---
+++ internal/acctest/service/base/rate_limit_configuration.go
@@ -0,0 +1,88 @@
+// Copyright © 2025 Ping Identity Corporation
+
+package base
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/patrickcping/pingone-go-sdk-v2/management"
+	"github.com/pingidentity/terraform-provider-pingone/internal/acctest"
+	"github.com/pingidentity/terraform-provider-pingone/internal/acctest/legacysdk"
+)
+
+func RateLimitConfiguration_CheckDestroy(s *terraform.State) error {
+	var ctx = context.Background()
+
+	p1Client, err := legacysdk.TestClient(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	apiClient := p1Client.API.ManagementAPIClient
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "pingone_rate_limit_configuration" {
+			continue
+		}
+
+		shouldContinue, err := legacysdk.CheckParentEnvironmentDestroy(ctx, apiClient, rs.Primary.Attributes["environment_id"])
+		if err != nil {
+			return err
+		}
+
+		if shouldContinue {
+			continue
+		}
+
+		_, r, err := apiClient.RateLimitingApi.ReadOneRateLimitConfiguration(ctx, rs.Primary.Attributes["environment_id"], rs.Primary.ID).Execute()
+
+		shouldContinue, err = acctest.CheckForResourceDestroy(r, err)
+		if err != nil {
+			return err
+		}
+
+		if shouldContinue {
+			continue
+		}
+
+		return fmt.Errorf("PingOne Rate Limit Configuration %s still exists", rs.Primary.ID)
+	}
+
+	return nil
+}
+
+func RateLimitConfiguration_GetIDs(resourceName string, environmentID, resourceID *string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+
+		rs, ok := s.RootModule().Resources[resourceName]
+		if !ok {
+			return fmt.Errorf("Resource Not found: %s", resourceName)
+		}
+
+		if resourceID != nil {
+			*resourceID = rs.Primary.ID
+		}
+
+		if environmentID != nil {
+			*environmentID = rs.Primary.Attributes["environment_id"]
+		}
+
+		return nil
+	}
+}
+
+func RateLimitConfiguration_RemovalDrift_PreConfig(ctx context.Context, apiClient *management.APIClient, t *testing.T, environmentID, rateLimitConfigurationID string) {
+	if environmentID == "" || rateLimitConfigurationID == "" {
+		t.Fatalf("One of environment ID or rate limit configuration ID cannot be determined. Environment ID: %s, Rate Limit Configuration ID: %s", environmentID, rateLimitConfigurationID)
+	}
+
+	_, err := apiClient.RateLimitingApi.DeleteRateLimitConfiguration(ctx, environmentID, rateLimitConfigurationID).Execute()
+	if err != nil {
+		t.Fatalf("Failed to delete rate limit configuration: %v", err)
+	}
+}
