cfef6d27e22b4be8f9aaabb791a51017c4052222
jonathanfuller-pingidentity <jonathanfuller@pingidentity.com>
2025-12-09 12:19:38 -0600

    Add more detail to error message

--- internal/framework/boolvalidator/bool_must_be_value_if_path_set_to_value.go
+++ internal/framework/boolvalidator/bool_must_be_value_if_path_set_to_value.go
@@ -87,7 +87,7 @@ func (v boolMustBeValueIfPathSetToValue) ValidateBool(ctx context.Context, req v
 	if matchedValue {
 		resp.Diagnostics.Append(validatordiag.InvalidAttributeCombinationDiagnostic(
 			req.Path,
-			v.Description(ctx),
+			fmt.Sprintf("Attribute %s must be true when attribute %v value is %s", req.Path, v.PathExpression, v.PathAttributeValue.ValueString()),
 		))
 	}
 }
