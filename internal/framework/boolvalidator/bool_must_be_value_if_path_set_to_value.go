47da6ef42eb5bbc289eaf70b8d71d81c46253bc4
jonathanfuller-pingidentity <jonathanfuller@pingidentity.com>
2026-01-06 15:56:03 -0600

    Fix validator message formatting

--- internal/framework/boolvalidator/bool_must_be_value_if_path_set_to_value.go
+++ internal/framework/boolvalidator/bool_must_be_value_if_path_set_to_value.go
@@ -87,7 +87,7 @@ func (v boolMustBeValueIfPathSetToValue) ValidateBool(ctx context.Context, req v
 	if matchedValue {
 		resp.Diagnostics.Append(validatordiag.InvalidAttributeCombinationDiagnostic(
 			req.Path,
-			fmt.Sprintf("Attribute %s must be true when attribute %v value is %s", req.Path, v.PathExpression, v.PathAttributeValue.ValueString()),
+			fmt.Sprintf("Attribute %s must be %t when attribute %v value is %s", req.Path, v.BoolValue.ValueBool(), v.PathExpression, v.PathAttributeValue.ValueString()),
 		))
 	}
 }
