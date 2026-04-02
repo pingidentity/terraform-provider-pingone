1cc13fc3e95ca37a7ba62c194302c3f76e663362
Henry Recker <henryrecker@pingidentity.com>
2026-04-02 11:43:09 -0500

    Format test HCL

--- internal/service/davinci/resource_davinci_flow_deploy_test.go
+++ internal/service/davinci/resource_davinci_flow_deploy_test.go
@@ -508,7 +508,7 @@ resource "pingone_davinci_connector_instance" "%[2]s-errors" {
 resource "pingone_davinci_flow" "%[2]s" {
   environment_id = data.pingone_environment.general_test.id
 
-	name  = "%[2]s-broken-flow"
+  name  = "%[2]s-broken-flow"
   color = "#FFC8C1"
 
   graph_data = {
