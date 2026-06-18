e7a8b00ee499a2b58ef56ab40d4af1f756a4d148
Peter Lambrechtsen <peter@crypt.co.nz>
2026-06-19 09:05:50 +1200

    Remove RequiresReplace on resource_environment as changing tags should not force the whole environment to be re-deployed. Also added optional for default so that the default FIDO2 policy can be set.

--- internal/service/base/resource_environment.go
+++ internal/service/base/resource_environment.go
@@ -19,7 +19,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/setplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
@@ -327,10 +326,6 @@ func (r *EnvironmentResource) Schema(ctx context.Context, req resource.SchemaReq
 							ElementType: types.StringType,
 
 							Optional: true,
-
-							PlanModifiers: []planmodifier.Set{
-								setplanmodifier.RequiresReplace(),
-							},
 
 							Validators: []validator.Set{
 								setvalidator.ValueStringsAre(
